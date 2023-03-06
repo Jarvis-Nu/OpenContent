aa2092cecf16313696d1ec0c667363f5976da5ed
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-06 23:51:34 +0100

    suprememods

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,6 +1,4 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Head from 'next/head';
-import connectContract from "../utils/connectContract"
 
 const Home = () => {
   return (
@@ -10,13 +8,6 @@ const Home = () => {
         <link href="/favicon.ico" rel="icon" />
       </Head>
       <main>
-        <ConnectButton />
-        <button onClick={async() => {
-          const date = new Date()
-          const web3cmsContract = connectContract()
-          const txn = await web3cmsContract.createNewBlogPost(["contentName","contentName","contentName","contentName","contentName","contentName","contentName",])
-          console.log(txn.wait)
-        }}>Click me</button>
       </main>
     </div>
   );
