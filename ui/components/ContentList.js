a6a194419dc7c365cadf8f6549aa7c0d3126456a
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-07 22:24:15 +0100

    suprememods

--- ui/components/ContentList.js
+++ ui/components/ContentList.js
@@ -1,12 +1,24 @@
 import ContentItem from "./ContentItem";
 import { useEffect, useState } from "react";
 import { useSigner } from 'wagmi';
-import Loader from "./Loader"
+import Loader from "./Loader";
+import axios from "axios"
 
 export default function ContentList() {
     const [address, setAddress] = useState(null)
     const [posts, setPosts] = useState([{}])
     const { data: signer } = useSigner()
+
+    async function fetchData() {
+        const result = await axios.get(`/api/openapiblog?address=${address}`)
+        return result
+    }
+
+    async function getPosts() {
+        const res = await fetchData()
+        console.log(res)
+        setPosts(res.data)
+    }
   
     async function getLoggedIn() {
       let userAddress = (await signer?.getAddress())?.toString()
@@ -16,16 +28,6 @@ export default function ContentList() {
       }
     }
 
-    async function fetchData() {
-        const result = await fetch(`/api/openapiblog?address=${address}`)
-        return result.json()
-    }
-
-    async function getPosts() {
-        const res = await fetchData()
-        setPosts(res.data?.blogPosts)
-    }
-  
     useEffect(() => {
       async() => getLoggedIn()
     }, [address])
@@ -38,7 +40,7 @@ export default function ContentList() {
             </div>
             <div className="flex flex-col space-y-1.5 max-h-screen overflow-y-scroll scrollbar-thin scrollbar-track-slate-100 scrollbar-thumb-[#DCD9CD] pb-5">
                 {
-                    address && posts ? posts.map(post => <ContentItem key={post?.id} title={post?.nameOfPost} id={post?.id} />) :
+                    address && posts.length > 0 ? posts.map(post => <ContentItem key={post?.id} title={post?.nameOfPost} id={post?.id} />) :
                     <div className="flex flex-col items-center space-y-1.5">
                         <Loader />
                     </div> 
