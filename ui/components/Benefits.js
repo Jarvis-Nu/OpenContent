2c6aa91bf07d73d7cfa6a06a0f2ae991c581445f
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-13 07:59:53 +0100

    suprememods

--- ui/components/Benefits.js
+++ ui/components/Benefits.js
@@ -1,19 +1,42 @@
+import Image from "next/image"
 import { useState } from "react"
 import ReactCardFlip from "react-card-flip"
 
 export default function Benefits() {
     const [isFlipped1, setIsFlipped1] = useState(false)
     return(
-        <div className="py-10 text-center">
-            <div className="w-full px-[25px] sm:px-[50px] lg:px-[100px] xl:px-0 py-4 sm:py-6 md:py-8 lg:py-10">
+        <div className="text-center px-[25px] sm:px-[50px] lg:px-[100px] xl:px-0 py-4 sm:py-6 md:py-8 lg:py-10 flex justify-center">
+            <div className="w-full max-w-6xl space-y-6">
                 <div>
                     <h3 className="text-dark">Benefits of using OpenContent</h3>
                 </div>
                 <div>
                     <div id="card_1" className="w-40" onMouseEnter={() => setIsFlipped1(true)} onMouseLeave={() => setIsFlipped1(false)}>
                         <ReactCardFlip isFlipped={isFlipped1} flipDirection="horizontal" flipSpeedBackToFront={1} flipSpeedFrontToBack={1}>
-                            <div className="bg-white border-2 border-dark/50 w-40 h-40 rounded-md">HEHEHE</div>
-                            <div className="bg-dark w-40 h-40 rounded-md">HOHOHOHO</div>
+                            <div className="bg-white border-2 border-dark/50 w-[275px] md:w-[300px] lg:w-[350px] min-h-[300px] rounded-md text-center hover:cursor-pointer">
+                                <div className="w-full h-full p-4 space-y-2">
+                                    <div className="flex justify-start">
+                                        <div className="relative">
+                                            <div>
+                                                <Image src={"/log_1.png"} width={80} height={80} alt="logo" />
+                                            </div>
+                                            <div className="absolute top-6 inset-x-6">
+                                                <Image src={"/log_2.png"} width={35} height={35} alt="logo" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h6 className="font-semibold">Free and Open-Source Content Management System</h6>
+                                    <p className="text-base">
+                                        OpenContent is a free and open-source content management system. 
+                                        You don't need to pay any licensing fees to use it, and it's completely 
+                                        customizable to suit your needs. With OpenContent, you can save money 
+                                        while still getting a powerful and secure CMS.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="bg-dark w-[275px] md:w-[300px] h-[300px] lg:w-[350px] rounded-md hover:cursor-pointer">{
+                                //back design
+                            }</div>
                         </ReactCardFlip>
                     </div>
                     <div></div>
--- ui/components/Hero.js
+++ ui/components/Hero.js
@@ -6,9 +6,9 @@ export default function Hero() {
         <div className="w-full bg-dark h-full px-[25px] sm:px-[50px] lg:px-[100px] xl:px-0 space-y-5 text-center pb-5 sm:pb-24 flex flex-col items-center">
             <div className="space-y-5 flex flex-col lg:flex-row lg:items-center lg:justify-between lg:space-y-0 lg:space-x-5 xl:space-x-16 max-w-6xl">
                 <div className="text-center lg:text-start space-y-5 lg:max-w-md xl:max-w-xl">
-                    <p className="text-red text-sm sm:text-base tracking-wide pt-5">The Decentralized Solution</p>
+                    <p className="text-red text-sm sm:text-base tracking-wide pt-5 font-[500]">The Decentralized Solution</p>
                     <h3 className="text-white">Secure Your Content with OpenContent</h3>
-                    <p className="text-ash text-[14px]">
+                    <p className="text-ash text-[14px] font-[500]">
                         OpenContent is powered by blockchain technology, which means that every change you make to your content 
                         is recorded in an immutable ledger. This provides unparalleled transparency and accountability, giving 
                         you complete control over your content.
