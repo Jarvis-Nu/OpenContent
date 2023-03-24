61aee29ec56bd165c3673bae45b8b0a64ba0e055
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-24 23:41:07 +0100

    suprememods

--- ui/components/Sponsors.js
+++ ui/components/Sponsors.js
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Sponsors() {
     return(
-        <div className="bg-dark/20 h-[200px] w-full">
+        <div className="bg-dark/20 py-5 space-y-5 w-full">
+            <div className="w-full text-center">
+                <h3>Our Sponsors</h3>
+            </div>
             <div className="w-full h-full flex space-x-[40px] justify-center items-center">
                 <div className="w-[100px] h-[100px] relative">
                     <Image src={"/polygon.png"} layout="fill" alt="sponsor logo" />
