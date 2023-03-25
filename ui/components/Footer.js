8967b65a889293dbf351d2776a283c2206afc49c
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-25 01:24:40 +0100

    suprememods

--- ui/components/Footer.js
+++ ui/components/Footer.js
@@ -2,7 +2,7 @@ import NigerianFlag from "./NigerianFlag"
 
 const Footer = () => {
   return (
-    <div className="w-full text-center py-5 bg-black text-white">Copyright @ 2023 OpenContent Ltd. All Rights Reserved</div>
+    <div className="w-full text-center py-5 bg-black text-white">Built by Nigerians {"-"} Owned by anyone</div>
   )
 }
 
