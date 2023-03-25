bbf80646a7d7b75f2f6a3b22d260480c0247a301
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-25 01:22:24 +0100

    suprememods

--- ui/components/ReversedBlock.js
+++ ui/components/ReversedBlock.js
@@ -1,26 +1,21 @@
 import Image from "next/image";
 import Button from "./Button";
 
-export default function ReversedBlock() {
+export default function ReversedBlock({ subtitle, title, description, image }) {
     return(
         <div className="w-full px-[25px] sm:px-[50px] lg:px-[100px] py-4 sm:py-6 md:py-8 lg:py-10 flex justify-center">
             <div className="max-w-6xl">
                 <div className="w-full flex flex-col space-y-2.5 sm:space-y-20 md:items-center lg:space-y-0 lg:flex-row lg:space-x-16">
                     <div className="text-start space-y-2.5 lg:max-w-md">
-                        <p className="text-red font-[500]">Blog templates</p>
-                        <h3 className="font-semibold">Professional Blog Templates</h3>
+                        <p className="text-red font-[500]">{subtitle}</p>
+                        <h3 className="font-semibold">{title}</h3>
                         <p>
-                            With OpenContent{"'"}s blog templates, you can easily create a stunning and functional blog. 
-                            Whether you{"'"}re a professional blogger or just starting out, our templates provide 
-                            a variety of options to suit your needs.
-                            From classic blog layouts to modern designs, our templates make it easy to create a blog that stands out.
+                            {description}
                         </p>
                         <Button text={"Get started"} />
                     </div>
-                    <div className="max-w-xl">
-                        <div className="relative max-h-[250px] max-w-[500px]">
-                            <Image src={"/pic_1.png"} alt="image" width={500} height={300} />
-                        </div>
+                    <div className="relative max-h-[250px] max-w-[500px]">
+                        <Image src={image} alt="image" width={500} height={300} />
                     </div>
                 </div>
             </div>
--- ui/pages/index.js
+++ ui/pages/index.js
@@ -19,9 +19,38 @@ const Home = () => {
         <Hero />
         <Benefits />
         <Sponsors />
-        <Block />
-        <ReversedBlock />
-        <Block />
+        <Block 
+          subtitle={"Blog templates"} 
+          title={"Professional Blog Templates"}
+          description={`
+            With OpenContent's blog templates, you can easily create a stunning and functional blog. 
+            Whether you're a professional blogger or just starting out, our templates provide a variety of options to suit your needs.
+            From classic blog layouts to modern designs, our templates make it easy to create a blog that stands out.
+          `} 
+          image={"/write.png"}
+        />
+        <ReversedBlock 
+          subtitle={"Vlogs templates"} 
+          title={"Engage Viewers with Vlogs"}
+          description={`
+            OpenContent's vlog templates offer a range of layouts and designs for video creators. 
+            Our templates provide options for showcasing your latest videos, highlighting your brand, 
+            and creating a unique online presence. With easy customization options, you can quickly 
+            create a vlog that represents your style and personality.
+          `} 
+          image={"/view.png"}
+        />
+        <Block 
+          subtitle={"Audio templates"} 
+          title={"Create with Audio Templates"}
+          description={`
+            OpenContent's audio templates provide an easy way to create a professional-looking website. 
+            Whether you're a podcaster or musician, our templates offer options for showcasing your latest 
+            audio content and engaging with your audience. With intuitive customization options, you can make 
+            your audio site look and feel just the way you want it to.
+          `} 
+          image={"/record.png"}
+        />
         <Footer />
       </main>
     </div>
