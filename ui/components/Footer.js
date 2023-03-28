f9a15258aaa9a89e160932fddbd59ea78c05f9ab
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-28 13:17:51 +0100

    suprememods

--- ui/components/Footer.js
+++ ui/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="w-full px-[25px] sm:px-[50px] lg:px-[100px] xl:px-0 py-4 sm:py-6 md:py-8 lg:py-10 flex justify-center">
       <div className="w-full max-w-6xl">
-        <div className="flex flex-col lg:flex-row justify-between space-y-1.5">
+        <div className="flex flex-col lg:flex-row justify-between space-y-1.5 lg:space-y-0 lg:space-x-5">
           <div className='w-full lg:w-80 space-y-1.5'>
             <p className="text-2xl font-semibold text-dark">OpenContent</p>
             <p>Enjoy the freedom of decentralization, the security of blockchain-backed data, and the convenience of prebuilt and customizable templates. Take your content management to the next level.</p>
