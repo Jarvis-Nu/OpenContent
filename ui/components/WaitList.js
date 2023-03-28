40d8f0ea7dd3439a0f913ed5212c59839a4a8317
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-28 11:31:02 +0100

    suprememods

--- ui/components/WaitList.js
+++ ui/components/WaitList.js
@@ -4,15 +4,15 @@ import Button from "./Button";
 export default function WaitList() {
     return(
         <div className="w-full px-[25px] sm:px-[50px] lg:px-[100px] py-4 sm:py-6 md:py-8 lg:py-10 flex justify-center">
-            <div className="w-full max-w-6xl px-5 py-10 bg-black rounded-md md:pl-10">
-                <div className="flex flex-col items-center w-full md:flex-row ">
-                    <div className="w-full h-full max-w-xl space-y-3 xl:space-y-5">
+            <div className="w-full max-w-6xl px-5 py-10 bg-black rounded-md md:pr-0 md:pl-10">
+                <div className="flex flex-col items-center w-full md:flex-row">
+                    <div className="w-full h-full max-w-xl space-y-3 xl:space-y-5 sm:pr-2.5 lg:pr-0">
                         <h2 className="text-xl font-semibold text-center text-white md:text-3xl xl:text-5xl md:text-start">Join the <span className="text-red">OpenContent</span> waitlist today and be the first to know.</h2>
                         <div className="text-center md:text-start">
                             <Button text={"Join Waitlist"} type={"filled"} href={"/"} />
                         </div>
                     </div>
-                    <div className="hidden w-full h-64 md:block">
+                    <div className="relative hidden w-full h-64 md:block">
                         <Image src={"/wait.png"} alt="image" layout="fill" />
                     </div>
                 </div>
